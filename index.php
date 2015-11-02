e9195acfcfd5eff72a954ce90219e1e0873a7d66
Nesar Ahammed Jony <jonybd34@gmail.com>
2015-11-02 19:20:44 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('MySqlDb.php');
+$Db = new MySqlDb('localhost', 'root', 'root', 'db');
+
+$Db->where('id', 34);
+$Db->delete('posts');
+
+
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <title>untitled</title>
+</head>
+<body>
+<pre>
+<?php print_r($results); ?>
+</pre>
+</body>
+</html>
